8f0eb16a6d6ea98ef5c2a8049f6382656536ff31
白仕云 <769290632@qq.com>
2018-06-11 15:28:39 +0800

    更新

--- BSYUI.podspec
+++ BSYUI.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name         = "BSYUI" 
-  s.version      = "1.1.2"     
+  s.version      = "1.1.3"     
   s.license      = "MIT"       
   s.summary      = "有没有希望写代码的时候像Masonry语法那样舒服简单，使用了连续的.语法。一直点点就可以了！BSYUI值一套针对常用控件的封装，支持链式编程，只需要继承自BSYUI中的控件，就可以实现啦！现在已经支持Cocoapods，希望对大家开发有帮助，也希望支持Star和Fork"
 
---
+++ BSYUI/BSYUI.h
@@ -0,0 +1,22 @@
+//
+//  BSYUI.h
+//  SpriteKit
+//
+//  Created by 白仕云 on 2018/6/11.
+//  Copyright © 2018年 BSY.com. All rights reserved.
+//
+
+#ifndef BSYUI_h
+#define BSYUI_h
+
+
+#endif /* BSYUI_h */
+
+#import "BSYButton.h"
+#import "BSYImageView.h"
+#import "BSYLabel.h"
+#import "BSYTableView.h"
+#import "BSYTableViewCell.h"
+#import "BSYTextField.h"
+#import "BSYView.h"
+
